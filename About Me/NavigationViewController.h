b10a2e7fa8d2c37cd9723961a03d8eeab044da88
Mason Wolters <masonw919@gmail.com>
2013-04-25 18:32:49 -0400

    Navigation

---
+++ About Me/NavigationViewController.h
@@ -0,0 +1,14 @@
+//
+//  NavigationViewController.h
+//  About Me
+//
+//  Created by Mason Wolters on 4/25/13.
+//  Copyright (c) 2013 Big Head Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ButtonNavigationView.h"
+
+@interface NavigationViewController : UINavigationController
+
+@end
---
+++ About Me/NavigationViewController.m
@@ -0,0 +1,42 @@
+//
+//  NavigationViewController.m
+//  About Me
+//
+//  Created by Mason Wolters on 4/25/13.
+//  Copyright (c) 2013 Big Head Apps. All rights reserved.
+//
+
+#import "NavigationViewController.h"
+
+@interface NavigationViewController ()
+
+@end
+
+@implementation NavigationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    ButtonNavigationView *buttonNav = [[ButtonNavigationView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:buttonNav];
+    
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
